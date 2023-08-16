9c0c5e1fd809a3bf036105164f01130ee5a59113
Abdullah Mohammed <37706742+AbdullahJ1445@users.noreply.github.com>
2023-08-16 12:03:08 -0600

    A

--- lib/entity/DoctorProfile.dart
+++ lib/entity/DoctorProfile.dart
@@ -1,41 +1,61 @@
-
 import 'package:booking_calendar/booking_calendar.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 import 'HealthCarePhacility.dart';
 import 'package:doctorppp/entity/userProfile.dart';
 
-
 class DoctorProfile extends UserProfile {
-
-  String?id;
+  String? id;
   String speciality;
   String clinicId;
 
-  
+  DoctorProfile(
+      {role,
+      fName,
+      lName,
+      email,
+      address,
+      phone,
+      meets,
+      picture,
+      required this.speciality,
+      required this.clinicId})
+      : super(
+            role: role,
+            fName: fName,
+            lName: lName,
+            email: email,
+            address: address,
+            phone: phone);
 
-  DoctorProfile({role,fName,lName,email,address,phone,meets,picture, required this.speciality ,required this.clinicId }) : super(role:role, fName:fName , lName:lName, email: email, address: address, phone:phone, picture: picture);
-
-  factory DoctorProfile.empty()=>DoctorProfile(clinicId: "",speciality: "",role: "",fName: "",lName: "",email: "",address: "",phone: "",picture: "");
-
+  factory DoctorProfile.empty() => DoctorProfile(
+      clinicId: "",
+      speciality: "",
+      role: "",
+      fName: "",
+      lName: "",
+      email: "",
+      address: "",
+      phone: "",
+      picture: "");
 
   factory DoctorProfile.fromJson(Map<String, dynamic> json) => DoctorProfile(
-    fName: json["fName"],
-    lName:json["lName"],
-    email:json["email"],
-    address:json["address"],
-    phone: json["pNumber"],
-    meets:json["meets"]!=null ? List<BookingService>.from(json["meets"].map((x) => BookingService.fromJson(x))):null,
-    role: json["role"],
-    picture: json["picture"],
-    speciality: json["speciality"],
-    clinicId: json["clinicId"],
-  );
+        fName: json["fName"],
+        lName: json["lName"],
+        email: json["email"],
+        address: json["address"],
+        phone: json["pNumber"],
+        meets: json["meets"] != null
+            ? List<BookingService>.from(
+                json["meets"].map((x) => BookingService.fromJson(x)))
+            : null,
+        role: json["role"],
+        picture: json["picture"],
+        speciality: json["speciality"],
+        clinicId: json["clinicId"],
+      );
 
-
-  Map<String, dynamic> toJson() =>
-
-      {
+  Map<String, dynamic> toJson() => {
         'role': role,
         'fName': fName,
         'lName': lName,
@@ -45,12 +65,6 @@ class DoctorProfile extends UserProfile {
         'meets': meets,
         'picture': picture,
         'speciality': speciality,
-        'clinicId':clinicId,
-
-
+        'clinicId': clinicId,
       };
-
-
 }
-
-
